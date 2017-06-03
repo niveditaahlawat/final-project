779570d567f58ec55b647829bc278282baab4bd8
niveditaahlawat <niveditaahlawat@ucla.edu>
2017-06-03 14:50:25 -0700

    test: S shape
    testing S shape on output window

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -1,6 +1,29 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
 
+const int M = 20;
+const int N = 10;
+
+int grid[M][N] = { 0 };
+
+
+class Point {
+public:
+	int x;
+	int y;
+}
+a[4], b[4];
+
+int shapes[7][4] = {
+	3, 5, 4, 6,	// S shape
+	2, 4, 5, 7, // Z shape
+	3, 5, 4, 7, // T shape
+	2, 3, 5, 7, // L shape
+	1, 3, 5, 7, // Line shape
+	3, 5, 7, 6,	// J shape
+	2, 3, 4, 5,	// Square shape
+};
+
 int main() {
 	// create the window for the tetris game
 	sf::RenderWindow window(sf::VideoMode(320, 480), "Welcome to Tetris!");
@@ -18,9 +41,20 @@ int main() {
 			if (game.type == sf::Event::Closed)
 				window.close();
 		}
+
+		int n = 0;
+		for (int i = 0; i < 4; ++i) {
+			a[i].x = shapes[n][i] % 2;
+			a[i].y = shapes[n][i] / 2;
+		}
+
 		// clear the window when the window is opened
 		window.clear(sf::Color::Black);
-		window.draw(sprite);	// test the texture
+
+		for (int i = 0; i < 4; ++i) {
+			sprite.setPosition(a[i].x * 18, a[i].y * 18);
+			window.draw(sprite);	// test the texture
+		}
 		window.display();
 	}
 	return 0;
