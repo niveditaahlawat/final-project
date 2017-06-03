3ff2e2da87b271655cab9cb3ebdfb98599a94014
niveditaahlawat <niveditaahlawat@ucla.edu>
2017-06-03 13:56:02 -0700

    created window for game
    used RenderWindow to create a window for the tetris game, which is
    initially set to a black screen when opened

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -1,5 +1,21 @@
 #include <SFML/Graphics.hpp>
+#include <time.h>
 
 int main() {
-
+	// create the window for the tetris game
+	sf::RenderWindow window(sf::VideoMode(320, 480), "Welcome to Tetris!");
+	
+	while (window.isOpen()) {
+		sf::Event game;
+		while (window.pollEvent(game))
+		{
+			// if the game is closed, close the window
+			if (game.type == sf::Event::Closed)
+				window.close();
+		}
+		// clear the window when the window is opened
+		window.clear(sf::Color::Black);
+		window.display();
+	}
+	return 0;
 }
